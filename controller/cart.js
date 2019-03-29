8bd89c22af2c27c20ed7d25f49a7bd0e992d4850
Fadli Ramadhan <fadlir93@gmail.com>
2019-03-29 13:49:50 +0700

    List all item in cart per ID

--- controller/cart.js
+++ controller/cart.js
@@ -1,30 +1,31 @@
 const db = require('../config/db_config')
 const Product = db.product
 const Cart = db.cart
-const Member = db.member
 exports.addItem = (req, res) => {
-    Member.findOne({
-      where: {id: req.userId}
-    }).then(() => {
-        Cart.create({
-          quantity: req.body.quantity,
-          memberId: req.userId,
-          productId: req.body.productId
-        })
-        .then(() => {
-          res.send("Item Success added to cart")
-        })
-      .catch(err => {
-          res.send(err)
-      })
-    }).catch(err => {
-      res.status(500).json({
-        "description": "Can not access User Page",
-        "error": err
-      });
-    })
+  Cart.create({
+    quantity: req.body.quantity,
+    memberId: req.userId,
+    productId: req.body.productId
+  }).then(() => {
+      res.send("Item Success added to cart")
+  }).catch(err => {
+      res.send(err)
+  })
 }
 
+exports.listItem = (req, res) => {
+  Cart.findAll({
+    memberId : req.userId,
+    attributes : [],
+    include : [{
+        model : db.product
+    }]
+  }).then((list) => {
+    res.send(list)      
+  }).catch(err => {
+      res.send(err)
+  })
+}
 // exports.addItem = (req, res) => {
 //   Cart.create({
 //     quantity: req.body.quantity,
--- routes/Cart.js
+++ routes/Cart.js
@@ -2,4 +2,5 @@ module.exports = (app) => {
     const Cart = require('../controller/cart')
     const verifyjwt = require('../validation/verifyJwtToken')
     app.post('/api/cart/additem', [verifyjwt], Cart.addItem)
+    app.get('/api/cart/listitems', [verifyjwt], Cart.listItem)
 }
