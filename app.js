cf87f2ad67882a8b4523db4c86d64074bfd64a39
Fadli Ramadhan <fadlir93@gmail.com>
2019-04-05 10:32:52 +0700

    function navbar when login or not

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session()); 
 
+app.use(function(req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
+
 app.use(flash())
 
 let indexRouter = require('./routes/Index')
